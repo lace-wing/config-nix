d98d1aaf2f86284d00fe5c7176cddbbe884750d0
lace-wing <lycrlsu01@gmail.com>
2026-06-10 10:32:29 -0700

    kmscon: colortheme

--- users/lacewing/nixos.nix
+++ users/lacewing/nixos.nix
@@ -39,12 +39,25 @@
     extraConfig = ''
       font-size=20
 
-      # grab-zoom-in=<Logo>Plus
-      # grab-zoom-out=<Logo>Minus
-      # grab-scroll-up=<Logo>Up
-      # grab-scroll-down=<Logo>Down
-      # grab-page-up=<Logo>Prior
-      # grab-page-down=<Logo>Next
+      palette=custom
+      palette-black=35,35,41
+      palette-blue=99,150,204
+      palette-cyan=222,170,187
+      palette-green=118,139,101
+      palette-magenta=134,129,173
+      palette-red=191,106,115
+      palette-light-grey=227,226,227
+      palette-yellow=218,187,90
+      palette-dark-grey=59,56,68
+      palette-white=245,243,245
+      palette-light-blue=184,214,230
+      palette-light-yellow=247,239,159
+      palette-light-cyan=229,202,224
+      palette-light-green=157,193,161
+      palette-light-red=187,64,69
+      palette-light-magenta=180,141,187
+      palette-background=21,21,20
+      palette-foreground=227,226,227
     '';
   };
 }
